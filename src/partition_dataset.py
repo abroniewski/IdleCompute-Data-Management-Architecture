4fdcfabe16cf56af3ae4283c83ae3c8da7d794a1
abroniewski <abroniewski@gmail.com>
2022-05-25 05:14:25 +0200

    logic for paritioning on sample/dummy data complete

--- src/partition_dataset.py
+++ src/partition_dataset.py
@@ -20,12 +20,84 @@
 # namenode manage block assignments and use the balancer periodically if you want to keep your cluster evenly
 # distributed
 
+#%% import
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+import pandas as pd
 
-import pyarrow.parquet as pq
+appName = "Scala Parquet Example"
+master = "local"
 
+spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
+
+#%% load
 #TODO: iterate through folder structure to find file
-dataset = pq.read_table('../data/processed/2022/03/VKY001-001-AB12')
-parquet_file = pq.ParquetFile('../data/processed/2022/03/VKY001-001-AB12')
-print(parquet_file.metadata)
+ip = [[37, 38, 39]]
 workload = [[20, 30, 50]]
+
+data_location = '../data/processed/2022/03/VKY001-001-AB12'
+schedule = pd.read_csv('../data/admin/schedule.csv')
+
+
+dataset = spark.read.parquet(data_location)
+
+# ip_assignment = workload[0][0]
+
+# rdd1 = spark.sparkContext.parallelize(dataset)
+# rdd2 = dataset.rdd.map(lambda x: )
+
+#%%
+# df1=dataset.rdd.zipWithIndex().toDF()
+# df2=df1.select(cols("_1.*"),cols("_2").alias('increasing_id'))
+# df1.show(1)
+# print("done")
+#%%
+nums = list(range(0,1000001))
+print(len(nums))
+nums_rdd = spark.sparkContext.parallelize(nums)
+squared_nums = nums_rdd.map(lambda x: x ** 2)
+print(nums_rdd)
+rdd_index = squared_nums.zipWithIndex()
+rdd_index.take(5)
+
+#%%
+##########################
+###     Example of Key + MapReduce by Workload
+##########################
+# row_count = dataset.count()
+nums = list(range(1,100))  # create dummy test set
+row_count = len(nums)  # save length of test set
+nums_rdd = spark.sparkContext.parallelize(nums)  #create RDD
+
+number_of_rows1 = 0.2*row_count  # splitting number of rows for each subset
+number_of_rows2 = 0.3*row_count
+number_of_rows3 = 0.5*row_count
+
+first_lower = 0  #determinging upper/loser boundaries for each section of work
+first_upper= first_lower + int(number_of_rows1) - 1
+second_lower = first_upper + 1
+second_upper = second_lower + int(number_of_rows2) - 1
+third_lower = second_upper + 1
+third_upper = row_count-1
+print(row_count)
+print ([first_lower,first_upper],[second_lower,second_upper],[third_lower,third_upper])
+
+nums2 = nums_rdd.zipWithIndex()  # adding index to dataframe
+nums3 = nums2.map(lambda x: (x[1],x[0] ** 2))  # creating some visual difference between values and hey
+# TODO: There is a flaw in this logic, as only 1 and 2 are assigned to each row
+nums4 = nums3.map(lambda x: ((1 if x[0] < second_lower else (2 if (x[0] >= second_lower && x[0] < third_lower) else (3
+if x[0] >= third_lower else 4))),x[0],x[1]))  # logic to assign key.
+nums4.collect()  # show results
+# getPartition()  # will be called to send each row to different partition based on its key.
+
+#%%
+dataset_index = dataset.rdd.zipWithIndex()  # transform spark.DataFrame into RDD and add sequential index
+dataset_index2 = dataset_index.map(lambda x: (x[1],x[0]))  # flip the index and value to behave as key/value pair
+# dataset_index3 = dataset_index2.map(lambda x: (1 if x[1],x[0]))
+
+# map(lambda x: 'lower' if x < 3 else 'higher', lst)
+
+#%%
+print("done")
+spark.stop()
 
