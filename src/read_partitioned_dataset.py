9f073aed3f0b793eee20f4ba2b5e7088d25a0f9d
abroniewski <abroniewski@gmail.com>
2022-06-12 14:03:17 +0200

    initial commit

---
+++ src/read_partitioned_dataset.py
@@ -0,0 +1,33 @@
+#%% import
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+from pyspark.sql.functions import udf
+from pyspark.rdd import portable_hash
+
+import pandas as pd
+import re
+
+appName = "Scala Parquet Example"
+master = "local"
+
+spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
+
+data_location = 'data/partitioned/2022/03/VKY001-002-AB12'
+
+#%%
+##########################
+#     Parquet read and partition
+##########################
+# Read Parquet file
+def read_partitioned_dataset(file_location, idlehost_ip, workload):
+        dataset_rdd = spark.read.parquet(file_location).rdd
+        print(dataset_rdd.take(5))
+
+#%%
+read_partitioned_dataset(file_location=data_location, idlehost_ip=None, workload=None)
+print("done partition_scheduled_dataset")
+#%%
+
+spark.stop()
+print("spark session ended")
+
