74cbfb24bc949e8524726c373b7aae3916cb5c85
abroniewski <abroniewski@gmail.com>
2022-06-12 14:02:07 +0200

    fixed type and changed schedule name

--- src/machine_in_progress.py
+++ src/machine_in_progress.py
@@ -11,23 +11,21 @@ import pandas as pd
 
 
 def machine_in_progress_list():
-    
     file_path = '../data/admin/'
-    schedule = pd.read_csv(f"{file_path}schedule_new.csv")
+    schedule = pd.read_csv(f"{file_path}schedule.csv")
     
     machine_in_progress = []
     task_in_progress_indexes = [n for n,x in enumerate(schedule['status']) if x=='in_progress']
-    
     for k in task_in_progress_indexes:
         machines = schedule['idlehost_ip'][k].replace('[',' ').replace(']',' ').replace(',',' ').split()
+
         for t in machines:
             machine_in_progress.append(t)
-            
     machine_in_progress = list(dict.fromkeys(machine_in_progress))
     df = pd.DataFrame(columns=['IP'])
     df['IP'] = machine_in_progress
     
-    df.to_csv(f'{file_path}machine_in_progresse.csv', index=True)
+    df.to_csv(f'{file_path}machine_in_progress.csv', index=True)
 
 
 # In[ ]:
